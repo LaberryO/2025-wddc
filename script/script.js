1033a98844b33b6a16ff2aac40397a1a83c64b0c
LaberryO <laberryo0912@gmail.com>
2025-05-29 12:28:51 +0900

    D-1 완성

--- script/script.js
+++ script/script.js
@@ -1,7 +1,32 @@
+$(".gallery").hide();
+$(".modal").hide();
+
 $(".main-menu>li").mouseenter(function () {
     $(this).children(".sub-menu").stop().slideDown();
 });
 
 $(".main-menu>li").mouseleave(function () {
     $(this).children(".sub-menu").stop().slideUp();
+});
+
+$("#notice").click(function () {
+    $("#notice").addClass("focus");
+    $("#gallery").removeClass("focus");
+    $(".notice").show();
+    $(".gallery").hide();
+});
+
+$("#gallery").click(function () {
+    $("#notice").removeClass("focus");
+    $("#gallery").addClass("focus");
+    $(".gallery").show();
+    $(".notice").hide();
+});
+
+$("#notice01").click(function () {
+    $(".modal").show();
+});
+
+$("#modal-exit").click(function () {
+    $(".modal").hide();
 });
