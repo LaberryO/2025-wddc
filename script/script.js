657ab5eafade6873525972d6288831b9856da5ef
LaberryO <laberryo0912@gmail.com>
2025-05-30 11:28:47 +0900

    D-3 완료

--- script/script.js
+++ script/script.js
@@ -1,9 +1,48 @@
 $(".sub-menu").hide();
+$(".sub-menu-back").hide();
+$(".gallery").hide();
+$(".modal").hide();
 
 $(".main-menu>li").mouseenter(function () {
-    $(this).children(".sub-menu").stop().slideDown();
+    $(".sub-menu").show();
+    $(".sub-menu-back").show();
 });
 
 $(".main-menu>li").mouseleave(function () {
-    $(this).children(".sub-menu").stop().slideUp();
+    $(".sub-menu").hide();
+    $(".sub-menu-back").hide();
 });
+
+$("#notice").click(() => {
+    $(".notice").show();
+    $(".gallery").hide();
+    $("#notice").addClass("focus");
+    $("#gallery").removeClass("focus");
+});
+
+$("#gallery").click(() => {
+    $(".notice").hide();
+    $(".gallery").show();
+    $("#notice").removeClass("focus");
+    $("#gallery").addClass("focus");
+});
+
+$("#modal").click(() => {
+    $(".modal").show();
+});
+
+$("#exit").click(() => {
+    $(".modal").hide();
+});
+
+let count = 0;
+setInterval(() => {
+    if (count < 2) {
+        count++;
+    } else {
+        count = 0;
+    }
+
+    $(".slide ul li").eq(count).fadeIn(500);
+    $(".slide ul li").eq(count).siblings().fadeOut(500);
+}, 3000);
